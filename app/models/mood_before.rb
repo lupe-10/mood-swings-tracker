5a7f6c954dd2aeeaabae028d4549b6964a9949ec
Lucia diez <lupe.diez@gmail.com>
2021-04-19 15:32:42 -0300

    footer, navbar and flashes partials

--- app/models/mood_before.rb
+++ app/models/mood_before.rb
@@ -1,3 +1,5 @@
 class MoodBefore < ApplicationRecord
   belongs_to :patient, class_name: "patient", foreign_key: "patient_id"
+  validates :other_thoughts, length: {maximum: 240}
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- has_many :moods_before, class_name: "mood_befores"
- has_many :moods_after, class_name: "mood_afters"
- belongs_to :psychologist, class_name: "psychologist", foreign_key: "psychologist_id"
+has_many :moods_before, class_name: "mood_befores"
+has_many :moods_after, class_name: "mood_afters"
+belongs_to :psychologist, class_name: "psychologist", foreign_key: "psychologist_id"
 end
