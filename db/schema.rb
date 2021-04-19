3edaea760692f48a324447048676261966568330
Lucia diez <lupe.diez@gmail.com>
2021-04-19 13:31:22 -0300

    new migration to add psychologist info to psychologists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_160623) do
+ActiveRecord::Schema.define(version: 2021_04_19_162429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,11 @@ ActiveRecord::Schema.define(version: 2021_04_19_160623) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone_number"
+    t.string "address"
+    t.string "professional_registration"
     t.index ["email"], name: "index_psychologists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_psychologists_on_reset_password_token", unique: true
   end
