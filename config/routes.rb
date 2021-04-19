d5b816bbadc17b8f5b99a90955cfcc6850355095
Lucia diez <lupe.diez@gmail.com>
2021-04-19 11:46:58 -0300

    adding routes for before and after exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+
   devise_for :psychologists, path: 'psychologists', controller: { session: 'user/session' }
   devise_for :patients, path: 'patients', controller: { session: 'patient/session' }
   devise_for :admins, path: 'admins', controller: { session: 'admin/session' }
+  root to: 'pages#home'
+
+  resources :mood_befores, only: %i[new create show]   
+  resources :mood_afters, only: %i[new create show]  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
