1f4a9a051048ec9a260e0527818630b9823ebd00
nataly garcia <naty_garcioli@hotmail.com>
2021-04-19 14:49:23 -0500

    accepts_nested_attributes_for :mood_befores and after

--- app/models/mood_after.rb
+++ app/models/mood_after.rb
@@ -1,4 +1,4 @@
 class MoodAfter < ApplicationRecord
-  belongs_to :patient, class_name: "psychologist", foreign_key: "psychologist_id" 
+  belongs_to :patient, class_name: 'psychologist', foreign_key: "psychologist_id" 
   validates :other_thoughts, length: {maximum: 240}
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,12 @@
 class Patient < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-has_many :moods_before, class_name: "mood_befores"
-has_many :moods_after, class_name: "mood_afters"
-belongs_to :psychologist, class_name: "psychologist", foreign_key: "psychologist_id"
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  has_many :moods_before, class_name: "mood_befores"
+  has_many :moods_after, class_name: "mood_afters"
+  #esto es para que podamos almacenar a traves de los id de los moods a los patients.
+  accepts_nested_attributes_for :mood_afters
+  accepts_nested_attributes_for :mood_befores
+  belongs_to :psychologist, class_name: "psychologist", foreign_key: "psychologist_id"
 end
