9752c451b5e78ccb993941178320bdb71354ba6a
nataly garcia <naty_garcioli@hotmail.com>
2021-04-23 21:20:59 -0500

    added constants to before model/ trying put new coding constants

--- app/models/mood_before.rb
+++ app/models/mood_before.rb
@@ -1,5 +1,6 @@
 class MoodBefore < ApplicationRecord
   belongs_to :patient, class_name: "patient", foreign_key: "patient_id"
   validates :other_thoughts, length: {maximum: 240}
+  MOOD_BEFORE = ['Sad', 'Happy', 'Tired', 'Gloomy', "Relaxed"]
 
 end
